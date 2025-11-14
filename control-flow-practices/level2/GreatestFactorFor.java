97bf16219450279297c68965c71e60cc5700555c
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:43:18 +0530

    GreatestFactorFor program

---
+++ control-flow-practices/level2/GreatestFactorFor.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class GreatestFactorFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int greatest = 1;
+        for (int i = number - 1; i >= 1; i--) {
+            if (number % i == 0) {
+                greatest = i;
+                break;
+            }
+        }
+        System.out.println(greatest);
+    }
+}
