929a7b26c4bfd02c19752a0b86e5746fcc338524
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:31:21 +0530

    SquareSide2 program

---
+++ level2-practice-problem/SquareSide2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class SquareSide2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double p = input.nextDouble();
+        double side = p / 4;
+        System.out.println("The length of the side is " + side + " whose perimeter is " + p);
+    }
+}
