0f3c4eaa0d74c0eb9845bb7f742466e1c10e4e42
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:39:48 +0530

    NaturalSumWhile program

---
+++ control-flow-practices/level1/NaturalSumWhile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class NaturalSumWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if (n > 0) {
+            int formula = n * (n + 1) / 2;
+            int sum = 0, i = 1;
+            while (i <= n) {
+                sum += i;
+                i++;
+            }
+            System.out.println(formula + " " + sum);
+        }
+    }
+}
