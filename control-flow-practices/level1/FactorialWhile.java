785f591917e703b3a9e7df28b65345c3d4bdc1ea
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:39:55 +0530

    FactorialWhile program

---
+++ control-flow-practices/level1/FactorialWhile.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class FactorialWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int fact = 1, i = 1;
+        while (i <= n) {
+            fact *= i;
+            i++;
+        }
+        System.out.println(fact);
+    }
+}
