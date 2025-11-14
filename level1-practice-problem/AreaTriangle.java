9a6290f6594d3819c7b759e2bedcd8cb51bde76b
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:30:15 +0530

    AreaTriangle program

---
+++ level1-practice-problem/AreaTriangle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AreaTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double base = input.nextDouble();
+        double height = input.nextDouble();
+        double area = 0.5 * base * height;
+        System.out.println("The area of triangle is " + area);
+    }
+}
