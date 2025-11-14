845f1a57a2a1eafd534d25a5df4dd1707647eb95
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:19:34 +0530

    KilometersToMiles program

---
+++ day1/KilometersToMiles.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class KilometersToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double km = sc.nextDouble();
+        System.out.println(km * 0.621371);
+    }
+}
