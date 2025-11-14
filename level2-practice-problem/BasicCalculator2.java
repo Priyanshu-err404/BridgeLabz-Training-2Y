06f61888a0b1ab97861692378b3d88bfaf32c3dd
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:31:14 +0530

    BasicCalculator2 program

---
+++ level2-practice-problem/BasicCalculator2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class BasicCalculator2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double n1 = input.nextDouble();
+        double n2 = input.nextDouble();
+        double add = n1 + n2;
+        double sub = n1 - n2;
+        double mul = n1 * n2;
+        double div = n1 / n2;
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " + n1 + " and " + n2 + " is " + add + ", " + sub + ", " + mul + ", and " + div);
+    }
+}
