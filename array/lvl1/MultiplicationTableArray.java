1c084ea0e502252d09eae04440eee51fc477af7d
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 03:00:50 +0530

    MultiplicationTableArray program

---
+++ array/lvl1/MultiplicationTableArray.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class MultiplicationTableArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] table = new int[10];
+        for (int i = 1; i <= 10; i++) table[i-1] = n * i;
+        for (int i = 1; i <= 10; i++) System.out.println(n + " * " + i + " = " + table[i-1]);
+    }
+}
