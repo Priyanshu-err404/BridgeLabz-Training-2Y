ec87981e6269fa58c22faab3c4a67360b232356e
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:40:01 +0530

    OddEvenRange program

---
+++ control-flow-practices/level1/OddEvenRange.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class OddEvenRange {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0)
+                System.out.println(i + " even");
+            else
+                System.out.println(i + " odd");
+        }
+    }
+}
