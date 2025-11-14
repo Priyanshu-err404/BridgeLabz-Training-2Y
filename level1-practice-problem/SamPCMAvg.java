87fb7f110ce2412d350e58453c034dcce386798e
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:29:43 +0530

    SamPCMAvg program

---
+++ level1-practice-problem/SamPCMAvg.java
@@ -0,0 +1,6 @@
+public class SamPCMAvg {
+    public static void main(String[] args) {
+        double maths = 94, physics = 95, chemistry = 96;
+        System.out.println("Sam’s average mark in PCM is " + (maths + physics + chemistry) / 3);
+    }
+}
