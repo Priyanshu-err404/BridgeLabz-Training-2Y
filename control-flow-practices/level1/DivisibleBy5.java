aedfeda39e5e367abb92d571ab750c40213aa66f
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-15 02:39:11 +0530

    DivisibleBy5 program

---
+++ control-flow-practices/level1/DivisibleBy5.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class DivisibleBy5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println("Is the number " + n + " divisible by 5? " + (n % 5 == 0));
+    }
+}
