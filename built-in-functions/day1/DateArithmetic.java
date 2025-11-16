3149416ebc362e8d62009b60c45de9f7d62675fe
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-16 18:00:17 +0530

    DateArithmetic

---
+++ built-in-functions/day1/DateArithmetic.java
@@ -0,0 +1,10 @@
+import java.time.*;
+import java.util.Scanner;
+public class DateArithmetic {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        LocalDate d = LocalDate.parse(in.next());
+        LocalDate r = d.plusDays(7).plusMonths(1).plusYears(2).minusWeeks(3);
+        System.out.println(r);
+    }
+}
