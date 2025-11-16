66bbdf57b6935c367f26ec2e0c425d212fec7b93
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-16 18:00:13 +0530

    TimeZones

---
+++ built-in-functions/day1/TimeZones.java
@@ -0,0 +1,8 @@
+import java.time.*;
+public class TimeZones {
+    public static void main(String[] args) {
+        System.out.println(ZonedDateTime.now(ZoneId.of("GMT")));
+        System.out.println(ZonedDateTime.now(ZoneId.of("Asia/Kolkata")));
+        System.out.println(ZonedDateTime.now(ZoneId.of("America/Los_Angeles")));
+    }
+}
