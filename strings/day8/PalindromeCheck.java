02c81c4187ddf7b66d6cec2e357ae91804188dc6
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-16 17:39:34 +0530

    PalindromeCheck

---
+++ strings/day8/PalindromeCheck.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class PalindromeCheck {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        String s=in.nextLine();
+        String r="";
+        for(int i=s.length()-1;i>=0;i--)r+=s.charAt(i);
+        System.out.println(s.equals(r));
+    }
+}
