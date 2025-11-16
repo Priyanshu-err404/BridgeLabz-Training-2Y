f695f54e5e551f47fc44800d242d1b94ec08dea3
Priyanshu-err404 <rishirajyadav565@gmail.com>
2025-11-16 19:18:46 +0530

    ECommerce

--- encapsulation& access modefiers/lvl1/ECommercePlatform.java
+++ encapsulation& access modefiers/lvl1/ECommercePlatform.java
@@ -12,8 +12,7 @@ abstract class Product {
 }
 interface Taxable { double calculateTax(); String getTaxDetails(); }
 class Electronics extends Product implements Taxable {
-    public Electronics(String id,String name,double price){ super(id,name,ppriceFix(price)); }
-    private static double ppriceFix(double p){ return p; }
+    public Electronics(String id,String name,double price){ super(id,name,price); }
     public double calculateDiscount(){ return getPrice()*0.10; }
     public double calculateTax(){ return getPrice()*0.18; }
     public String getTaxDetails(){ return "GST 18%"; }
